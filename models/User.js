07a651ad2eb4063b5b90fb84215c8855c091eaf2
alexfortin <fortin.alex14@gmail.com>
2022-12-09 12:21:10 -0600

    created the user model

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const {Schema, default: mongoose} = require("mongoose")
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+/
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "thought"
+    }],
+    friends: [userSchema]
+},
+{
+    toJSON: {
+        virtuals: true
+    }
+})
+
+userSchema.virtual("friendCount").get(function(){
+    return this.friends.length
+})
+
+const User = mongoose.model("user", userSchema)
+
+module.exports = User
