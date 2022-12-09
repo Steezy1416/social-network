0316ffc9ef8394309779813bbb8f653818f14cb7
alexfortin <fortin.alex14@gmail.com>
2022-12-09 12:46:24 -0600

    created the server and can connect to database

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const moongoose = require("mongoose")
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+moongoose.connect("mongodb://127.0.0.1:27017/social-network",
+{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+
+app.listen(PORT, () => console.log("========== CONNECTED TO SERVER =========="))
